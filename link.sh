4637ce17cd81697f809cbcd2086eeae103092cf8
Shota HAMADA <maku3ts@gmail.com>
2017-03-01 00:01:41 +0900

    Fix installation shell script

--- link.sh
+++ link.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 cd `dirname "${0}"`
-files=`ls -1 | grep -v "link.sh"`
+files=`ls -1 *vimrc`
 for f in ${files}; do
     ln -si "${PWD}/${f}" "${HOME}/.${f}" && echo "${PWD}/${f}" "${HOME}/${f}"
 done
