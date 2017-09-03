b8c0cf78eeb203cb4913f0dc4a90d63dc4d99839
Shota HAMADA <maku3ts@gmail.com>
2017-09-03 13:35:18 +0900

    Fix link script's output

--- link.sh
+++ link.sh
@@ -2,5 +2,5 @@
 cd `dirname "${0}"`
 files=`ls -1 *vimrc`
 for f in ${files}; do
-    ln -si "${PWD}/${f}" "${HOME}/.${f}" && echo "${PWD}/${f}" "${HOME}/${f}"
+    ln -si "${PWD}/${f}" "${HOME}/.${f}" && echo "${PWD}/${f}" "${HOME}/.${f}"
 done
